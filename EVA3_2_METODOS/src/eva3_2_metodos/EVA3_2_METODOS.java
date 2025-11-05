7ce8a3adc0cdac754ccf6f82c1938c9f2961e155
Arath Cabral <censedgiraffed96@gmail.com>
2025-11-05 17:55:18 -0600

    Add files via upload

---
+++ EVA3_2_METODOS/src/eva3_2_metodos/EVA3_2_METODOS.java
@@ -0,0 +1,16 @@
+//ARATH 👍
+package eva3_2_metodos;
+
+public class EVA3_2_METODOS {
+
+    public static void main(String[] args) {
+        repetirMensaje("Hola", 5); //No regresa ningun valor
+    }
+    //Declaracion del metodo
+    public static void repetirMensaje (String mensaje, int repeticion){
+        for (int i = 1; i <= repeticion; i++){
+        System.out.println(mensaje);
+    }
+    }
+    }
+
